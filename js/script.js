cff186a01fadfdcda0c321acba008b07bb8e7e14
Carlos Vinícius <107522005+carlos-vini@users.noreply.github.com>
2022-12-14 19:19:15 -0300

    js adicionado para o modal

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function abrir(){
+
+    let modal = document.querySelector('.modal-index')
+
+    modal.style.display = 'flex';
+}
+
+function fechar(){
+
+    let modal = document.querySelector('.modal-index')
+
+    modal.style.display = 'none';
+}
